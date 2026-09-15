89ec527f0afe28d4ed3b6521c4615c4c36c25abf
ProfEntusiasta <prof_gabrielpozzo@camporeal.edu.br>
2026-09-15 14:32:01 -0300

    Arrumando models e fazendo os relacionamentos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,4 +13,14 @@ class Product extends Model
         'stock',
         'is_active',
     ];
+
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
